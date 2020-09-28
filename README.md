313d4f973ed991bbc87b75842e03f830a09c8478
Balaji Damodaran <damodaran.balaji@gmail.com>
2020-09-28 13:11:09 +0200

    use gradle to build

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # rF2-Manager
 
 Hobby Project to view and analyze rFactor2 logs.
+
+## setup
+* Need webview dependencies installed for respective OS-es (TODO: elaborate)
+* Install `yarn`
+* Build web locally
--- build.rs
+++
@@ -1,8 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    println!("cargo:rerun-if-changed=web/dist/index.html");
-    Command::new("yarn")
-        .args(&["--cwd", "web"])
-        .arg("build").status().unwrap();
-}
